0fe3379cc4fa2526ea01873f9da63e30c58b22a3
johnb8005 <johnb8005@users.noreply.github.com>
2020-08-14 00:06:03 +0000

    deploy: f7cfb966615bae670c4abeb8e5ab94f5fda405a5

---
+++ precache-manifest.dbc2b51cd8fc1f8ef48fe68acf7dbce8.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc3fb52cc653380ef6c4e3a1ed665694",
+    "url": "/react-nexys-boilerplate/index.html"
+  },
+  {
+    "revision": "9b25ad7210733a03c51d",
+    "url": "/react-nexys-boilerplate/static/js/2.06c9093f.chunk.js"
+  },
+  {
+    "revision": "dfedff5ba6df691e932b",
+    "url": "/react-nexys-boilerplate/static/js/main.2ec0c259.chunk.js"
+  },
+  {
+    "revision": "d0be0caae962ccd499f0",
+    "url": "/react-nexys-boilerplate/static/js/runtime-main.3c36c9b6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-nexys-boilerplate/precache-manifest.b5c4c7075fe4c7ba9197e2250b08aa13.js"
+  "/react-nexys-boilerplate/precache-manifest.dbc2b51cd8fc1f8ef48fe68acf7dbce8.js"
 );
 
 self.addEventListener('message', (event) => {
